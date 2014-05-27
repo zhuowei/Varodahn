28f299bf7e6d6cba38e30250ee98af45b6abd364
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2014-05-27 18:35:16 -0400

    Send desired video resolution during config, dump received video data to file

--- src/net/zhuoweizhang/varodahn/StreamingClient.java
+++ src/net/zhuoweizhang/varodahn/StreamingClient.java
@@ -3,6 +3,8 @@ package net.zhuoweizhang.varodahn;
 import java.io.*;
 import java.net.*;
 
+import java.util.Arrays;
+
 import com.google.protobuf.ByteString;
 import com.google.protobuf.GeneratedMessage;
 
@@ -15,6 +17,7 @@ public class StreamingClient implements Runnable {
 
 	private static final int PACKET_TYPE_CONNECT = 1;
 	private static final int PACKET_TYPE_CONNECT_RESPONSE = 2;
+	private static final int PACKET_TYPE_DATA = 3;
 	private static final int PACKET_TYPE_CONTROL = 5;
 	private static final int PACKET_TYPE_CONTROL_CONTINUED = 6;
 	private static final int PACKET_TYPE_CONTROL_ACKNOWLEDGE = 7;
@@ -32,6 +35,8 @@ public class StreamingClient implements Runnable {
 	private boolean connected = false;
 	private ByteArrayOutputStream splitPacketBuffer = new ByteArrayOutputStream();
 	private int splitPacketRemaining = 0;
+
+	private int[] channelAssignments = new int[128];
 
 	public StreamingClient(String serverAddr, int port, ByteString authToken) {
 		this.serverAddr = serverAddr;
@@ -59,6 +64,9 @@ public class StreamingClient implements Runnable {
 		socket = new DatagramSocket();
 		socket.connect(socketAddress);
 		connected = true;
+
+		Arrays.fill(channelAssignments, -1);
+
 		// do you wanna send a packet?
 		writeOpenConnectionMessage();
 	}
@@ -71,7 +79,10 @@ public class StreamingClient implements Runnable {
 	 * byte: repeat count for repeating dropped packets (stops incrementing after 0xff)
 	 * byte: sender ID
 	 * byte: receiver ID (0 when sending open connection packet)
-	 * byte: dunno: 0, 1, 2 sighted
+	 * byte: stream channel ID: see EStreamChannel in the protobuf. 0 is discovery (used in packet type 1, 2),
+		1 is control (used in many 5, 6, 7 packets),
+		2 is stats,
+		3 and above is used for data.
 	 * byte: usually 0, used for reconstructing split packets
 		For control channel this will be number of packets remaining to read; split packets will be send on channel 6
 		For the split packets on channel 6, this is a sequence number.
@@ -86,10 +97,10 @@ public class StreamingClient implements Runnable {
 	 */
 
 	private void writeOpenConnectionMessage() throws IOException {
-		writeRawMessage(1, 0, new byte[]{});
+		writeRawMessage(PACKET_TYPE_CONNECT, EStreamChannel.k_EStreamChannelDiscovery_VALUE, new byte[]{});
 	}
 
-	private void writeRawMessage(int packetType, int flags, byte[] payload) throws IOException {
+	private void writeRawMessage(int packetType, int dataChannel, byte[] payload) throws IOException {
 		// todo: splitting oversized packets.
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		DataOutputStream dos = new DataOutputStream(bos);
@@ -97,7 +108,7 @@ public class StreamingClient implements Runnable {
 		dos.write(0); //repeat count: todo
 		dos.write(senderId);
 		dos.write(receiverId);
-		dos.write(flags); //dunno
+		dos.write(dataChannel); //dunno
 		dos.writeShort(0); //dunno
 		dos.writeShort(Short.reverseBytes(packetSequenceIds[packetType]));
 		dos.writeInt(Integer.reverseBytes(getTimestamp()));
@@ -113,8 +124,8 @@ public class StreamingClient implements Runnable {
 		DatagramPacket packet = new DatagramPacket(byteBuf, byteBuf.length);
 		while(connected) {
 			socket.receive(packet);
-			loggingOut.write(byteBuf, 0, packet.getLength());
-			loggingOut.flush();
+			//loggingOut.write(byteBuf, 0, packet.getLength());
+			//loggingOut.flush();
 			System.out.println(Hex.toHexString(byteBuf, 0, packet.getLength()));
 			int packetType = byteBuf[0];
 			switch(packetType) {
@@ -141,6 +152,9 @@ public class StreamingClient implements Runnable {
 						processControlMessage(fullBuf, 0, fullBuf.length);
 					}
 					break;
+				case PACKET_TYPE_DATA:
+					System.out.println("Data!");
+					break;
 				case PACKET_TYPE_DISCONNECT:
 					connected = false;
 					break;
@@ -163,6 +177,25 @@ public class StreamingClient implements Runnable {
 
 	private void processControlMessage(byte[] buffer, int begin, int length) throws IOException {
 		sendControlAckMessage(buffer, begin);
+		int channel = buffer[begin+4];
+		if (channel >= EStreamChannel.k_EStreamChannelDataChannelStart_VALUE) {
+			int channelAssignment = channelAssignments[channel];
+			byte[] outmsg = new byte[4];
+			System.arraycopy(buffer, begin + 16, outmsg, 0, 4);
+			System.out.println("Acking " + Hex.toHexString(outmsg));
+			writeRawMessage(PACKET_TYPE_CONTROL_ACKNOWLEDGE, EStreamChannel.k_EStreamChannelControl_VALUE, outmsg);
+			switch (channelAssignment) {
+				case EStreamingDataType.k_EStreamingAudioData_VALUE:
+					processAudioControlMessage(buffer, begin, length);
+					break;
+				case EStreamingDataType.k_EStreamingVideoData_VALUE:
+					processVideoControlMessage(buffer, begin, length);
+					break;
+				default:
+					break;
+			}
+			return;
+		}
 		GeneratedMessage msg = readControlMessage(buffer, begin, length);
 		System.out.println(msg);
 		if (msg instanceof CNegotiationInitMsg) {
@@ -171,13 +204,23 @@ public class StreamingClient implements Runnable {
 				setConfig(CNegotiatedConfig.newBuilder().
 					setReliableData(false).
 					setSelectedAudioCodec(EStreamAudioCodec.k_EStreamAudioCodecVorbis).
-					setSelectedVideoCodec(EStreamVideoCodec.k_EStreamVideoCodecH264)
+					setSelectedVideoCodec(EStreamVideoCodec.k_EStreamVideoCodecH264).
+					addAvailableVideoModes(CStreamVideoMode.newBuilder().
+						setWidth(320).
+						setHeight(240)
+					)
 					// todo: un-hardcode
 				).
 				build();
 			writeControlMessage(setconfigMsg);
 		} else if (msg instanceof CNegotiationSetConfigMsg) {
 			writeControlMessage(CNegotiationCompleteMsg.getDefaultInstance());
+		} else if (msg instanceof CStartAudioDataMsg) {
+			CStartAudioDataMsg startMsg = (CStartAudioDataMsg) msg;
+			channelAssignments[startMsg.getChannel()] = EStreamingDataType.k_EStreamingAudioData_VALUE;
+		} else if (msg instanceof CStartVideoDataMsg) {
+			CStartVideoDataMsg startMsg = (CStartVideoDataMsg) msg;
+			channelAssignments[startMsg.getChannel()] = EStreamingDataType.k_EStreamingVideoData_VALUE;
 		}
 	}
 	private GeneratedMessage readControlMessage(byte[] buffer, int beginOfBuffer, int lengthOfBuffer) {
@@ -212,20 +255,32 @@ public class StreamingClient implements Runnable {
 		byte[] withId = new byte[serialized.length + 1];
 		withId[0] = (byte) escmsg;
 		System.arraycopy(serialized, 0, withId, 1, serialized.length);
-		writeRawMessage(PACKET_TYPE_CONTROL, 1, withId);
+		writeRawMessage(PACKET_TYPE_CONTROL, EStreamChannel.k_EStreamChannelControl_VALUE, withId);
 	}
 
 	private void sendControlAckMessage(byte[] originalMessage, int offset) throws IOException {
 		byte[] outmsg = new byte[4];
 		System.arraycopy(originalMessage, offset + 9, outmsg, 0, 4);
-		writeRawMessage(PACKET_TYPE_CONTROL_ACKNOWLEDGE, 1, outmsg);
+		byte channel = originalMessage[offset+4];
+		writeRawMessage(PACKET_TYPE_CONTROL_ACKNOWLEDGE, channel, outmsg);
 	}
 
 	private void writeAckMessageRaw(int timestamp) throws IOException {
 		byte[] data = new byte[4];
 		writeIntToByteArray(data, 0, timestamp);
-		writeRawMessage(PACKET_TYPE_CONTROL_ACKNOWLEDGE, 1, data);
+		writeRawMessage(PACKET_TYPE_CONTROL_ACKNOWLEDGE, EStreamChannel.k_EStreamChannelControl_VALUE, data);
 	}
+
+	private void processAudioControlMessage(byte[] msg, int offset, int length) throws IOException {
+		System.out.println("Ignoring audio control message");
+	}
+
+	private void processVideoControlMessage(byte[] msg, int offset, int length) throws IOException {
+		if (msg[offset+13] != 0x1) return;
+		System.out.println("writing video control message");
+		loggingOut.write(msg, offset+30, length-30);
+	}
+
 	private static void writeIntToByteArray(byte[] buffer, int offset, int value) {
 		buffer[offset++] = (byte) value;
 		buffer[offset++] = (byte) (value >> 8);
